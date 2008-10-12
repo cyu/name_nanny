1f142bdd16c91fa83411ffef4a1fdb8be44fa47d
Calvin Yu <calvinyu@calvin-yus-macbook-pro.local>
2008-10-12 10:23:18 -0400

    support loading words from rails project added wholesome? method to do just a word check

--- lib/name_nanny.rb
+++ lib/name_nanny.rb
@@ -1,7 +1,18 @@
 # The Name Nanny makes sure that users behave themselves when on the system.
 module NameNanny
-  RESERVED_NAMES = (IO.readlines RAILS_ROOT + '/vendor/plugins/name_nanny/lib/reserved_words.txt').each { |w| w.chop! }
-  BAD_WORDS      = (IO.readlines RAILS_ROOT + '/vendor/plugins/name_nanny/lib/bad_words.txt').each { |w| w.chop! }
+  BAD_WORDS_LOCATION = File.dirname(__FILE__) + '/bad_words.txt' 
+
+  def self.bad_words(*words)
+    all = [ get_words(BAD_WORDS_LOCATION) ]
+    words.inject(all) do |arr, v|
+      arr << get_words("#{RAILS_ROOT}/config/#{v.to_s}_words.txt") if v; arr
+    end
+  end 
+
+  def self.get_words(loc)
+    @@words_cache ||= {}
+    @@words_cache[loc] || (@@words_cache[loc] = IO.readlines(loc).collect(&:chop!))
+  end
   
   # Use a non-descript error to prevent the users from trying to hack around the filter.
   # Hopefully, they will just give up and choose something nicer.
@@ -11,29 +22,39 @@ module NameNanny
     
     validates_each(attr_names) do |record, attr_names|
       unless !configuration[:if].nil? and not configuration[:if].call(record)
-        record.errors.add( attr_names, configuration[:message] ) if bad_name? record.send(attr_names)
+        words = NameNanny.bad_words(*configuration[:words])
+        record.errors.add( attr_names, configuration[:message] ) if wholesome?(record.send(attr_names), words)
       end
     end
   end
 
-  def bleep_text(str)
-    sub_text(str,"bleeep")
+  def bleep_text(str,opts={})
+    sub_text(str,"bleeep",opts)
   end
 
-  def smurf_text(str)
-    sub_text(str,"smurf")
+  def smurf_text(str,opts={})
+    sub_text(str,"smurf",opts)
   end
 
-  def strip_text(str)
-    sub_text(str,"")
+  def strip_text(str,opts={})
+    sub_text(str,"",opts)
+  end
+
+  def wholesome?(str, words=[])
+    bad_name = false
+    words =  str.split(" ").each do |name|
+      bad_name = true if words.any? { |v| v.include?(name.downcase) }
+    end
+    bad_name
   end
 
   protected
-  def sub_text(str,replacement = "bleeep")
+  def sub_text(str,replacement = "bleeep", opts={})
+    bad_words = NameNanny.bad_words(*opts[:words])
     
     # Replace commas with an unlikely character combination
     str = str.gsub(',', ' ^&^ ')
-    baddies = str.split(" ").map { | word | word.rstrip if BAD_WORDS.include?(word.rstrip.downcase) }.compact
+    baddies = str.split(" ").map { | word | word.rstrip if bad_words.include?(word.rstrip.downcase) }.compact
     baddies.each { |word| str.gsub!(word, replacement) }
     
     # Return commas to their correct position within the string
@@ -41,12 +62,4 @@ module NameNanny
     str
   end
   
-  def bad_name?(str)
-    bad_name = false
-    words =  str.split(" ").each do |name|
-      bad_name = true if RESERVED_NAMES.include?(name.downcase)
-      bad_name = true if BAD_WORDS.include?(name.downcase)
-    end
-    bad_name
-  end
 end
